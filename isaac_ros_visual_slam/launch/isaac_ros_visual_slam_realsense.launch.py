d8f2f6e786d1cf852fb842ac27483a393fa12248
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2024-01-31 13:11:03 +0200

    add vscode settings and recording to launch file

--- isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense.launch.py
+++ isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense.launch.py
@@ -18,70 +18,104 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import EnvironmentVariable
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
+    record_arg = DeclareLaunchArgument(
+        "record",
+        default_value="false",
+        description="Enable recording of topics with rosbag",
+    )
+
     """Launch file which brings up visual slam node configured for RealSense."""
     realsense_camera_node = Node(
-        name='camera',
-        namespace='camera',
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        parameters=[{
-                'enable_infra1': True,
-                'enable_infra2': True,
-                'enable_color': False,
-                'enable_depth': False,
-                'depth_module.emitter_enabled': 0,
-                'depth_module.profile': '640x360x90',
-                'enable_gyro': True,
-                'enable_accel': True,
-                'gyro_fps': 200,
-                'accel_fps': 200,
-                'unite_imu_method': 2
-        }]
+        name="camera",
+        namespace="camera",
+        package="realsense2_camera",
+        executable="realsense2_camera_node",
+        parameters=[
+            {
+                "enable_infra1": True,
+                "enable_infra2": True,
+                "enable_color": False,
+                "enable_depth": False,
+                "depth_module.emitter_enabled": 0,
+                "depth_module.profile": "640x360x90",
+                "enable_gyro": True,
+                "enable_accel": True,
+                "gyro_fps": 200,
+                "accel_fps": 200,
+                "unite_imu_method": 2,
+            }
+        ],
     )
 
     visual_slam_node = ComposableNode(
-        name='visual_slam_node',
-        package='isaac_ros_visual_slam',
-        plugin='nvidia::isaac_ros::visual_slam::VisualSlamNode',
-        parameters=[{
-                    'denoise_input_images': False,
-                    'rectified_images': True,
-                    'enable_debug_mode': False,
-                    'debug_dump_path': '/tmp/cuvslam',
-                    'enable_slam_visualization': True,
-                    'enable_landmarks_view': True,
-                    'enable_observations_view': True,
-                    'map_frame': 'map',
-                    'odom_frame': 'odom',
-                    'base_frame': 'camera_link',
-                    'input_imu_frame': 'camera_gyro_optical_frame',
-                    'enable_imu_fusion': True,
-                    'gyro_noise_density': 0.000244,
-                    'gyro_random_walk': 0.000019393,
-                    'accel_noise_density': 0.001862,
-                    'accel_random_walk': 0.003,
-                    'calibration_frequency': 200.0,
-                    'img_jitter_threshold_ms': 22.00
-                    }],
-        remappings=[('stereo_camera/left/image', 'camera/infra1/image_rect_raw'),
-                    ('stereo_camera/left/camera_info', 'camera/infra1/camera_info'),
-                    ('stereo_camera/right/image', 'camera/infra2/image_rect_raw'),
-                    ('stereo_camera/right/camera_info', 'camera/infra2/camera_info'),
-                    ('visual_slam/imu', 'camera/imu')]
+        name="visual_slam_node",
+        package="isaac_ros_visual_slam",
+        plugin="nvidia::isaac_ros::visual_slam::VisualSlamNode",
+        parameters=[
+            {
+                "denoise_input_images": False,
+                "rectified_images": True,
+                "enable_debug_mode": False,
+                "debug_dump_path": "/tmp/cuvslam",
+                "enable_slam_visualization": True,
+                "enable_landmarks_view": True,
+                "enable_observations_view": True,
+                "map_frame": "map",
+                "odom_frame": "odom",
+                "base_frame": "camera_link",
+                "input_imu_frame": "camera_gyro_optical_frame",
+                "enable_imu_fusion": True,
+                "gyro_noise_density": 0.000244,
+                "gyro_random_walk": 0.000019393,
+                "accel_noise_density": 0.001862,
+                "accel_random_walk": 0.003,
+                "calibration_frequency": 200.0,
+                "img_jitter_threshold_ms": 22.00,
+            }
+        ],
+        remappings=[
+            ("stereo_camera/left/image", "camera/infra1/image_rect_raw"),
+            ("stereo_camera/left/camera_info", "camera/infra1/camera_info"),
+            ("stereo_camera/right/image", "camera/infra2/image_rect_raw"),
+            ("stereo_camera/right/camera_info", "camera/infra2/camera_info"),
+            ("visual_slam/imu", "camera/imu"),
+        ],
     )
 
     visual_slam_launch_container = ComposableNodeContainer(
-        name='visual_slam_launch_container',
-        namespace='',
-        package='rclcpp_components',
-        executable='component_container',
-        composable_node_descriptions=[
-            visual_slam_node
-        ],
-        output='screen'
+        name="visual_slam_launch_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[visual_slam_node],
+        output="screen",
     )
 
-    return launch.LaunchDescription([visual_slam_launch_container, realsense_camera_node])
+    record_rosbag = ExecuteProcess(
+        condition=IfCondition(LaunchConfiguration("record")),
+        cmd=[
+            "ros2",
+            "bag",
+            "record",
+            "-o",
+            "/datasets/debug/isaac_vslam/office_short_d455",
+            "camera/infra1/image_rect_raw",
+            "camera/infra1/camera_info",
+            "camera/infra2/image_rect_raw",
+            "camera/infra2/camera_info",
+            "camera/imu",
+            # ... [add any other topics you wish to record]
+        ],
+        shell=True,
+    )
+
+    return launch.LaunchDescription(
+        [record_arg, visual_slam_launch_container, realsense_camera_node, record_rosbag]
+    )
